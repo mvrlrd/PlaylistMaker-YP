e0d31da614bf1d9f34216b32f099ef50739d5024
mvrlrd <mvrlrd@yandex.ru>
2022-11-13 19:59:44 +0300

    доделал активити настроек

--- app/src/main/java/ru/mvrlrd/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,17 @@
 package ru.mvrlrd.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
     }
+
+
 }
