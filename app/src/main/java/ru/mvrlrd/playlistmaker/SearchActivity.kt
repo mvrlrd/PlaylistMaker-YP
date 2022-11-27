0d63218a85e6f075ae029d12f89e0c4d9242bfe0
mvrlrd <mvrlrd@yandex.ru>
2022-11-27 16:38:15 +0300

    сохраняю состояни при перевороте

--- app/src/main/java/ru/mvrlrd/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/SearchActivity.kt
@@ -6,16 +6,21 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.ImageView
 
+const val INPUT_TEXT = "INPUT_TEXT"
 class SearchActivity : AppCompatActivity() {
+    var text = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val searchEditText = findViewById<EditText>(R.id.searchEditText)
         val clearIcon = findViewById<ImageView>(R.id.clearIcon)
+
+        if (savedInstanceState!=null){
+            searchEditText.setText(savedInstanceState.getString(INPUT_TEXT))
+        }
 
         clearIcon.setOnClickListener {
             searchEditText.text.clear()
@@ -31,10 +36,20 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(p0: Editable?) {
-
+                text = p0.toString()
             }
         }
         searchEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(INPUT_TEXT, text)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        text = savedInstanceState.getString(INPUT_TEXT, "")
     }
 
     private fun clearButtonVisibility(p0: CharSequence?) = if (p0.isNullOrEmpty()) View.GONE else View.VISIBLE
