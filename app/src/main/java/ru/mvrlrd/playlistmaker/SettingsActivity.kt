1604416a5e6d220a60e67a6d780318537f42ffbc
mvrlrd <mvrlrd@yandex.ru>
2022-11-27 12:53:47 +0300

    реализовал переход по интентам Поддержка и пользовательское соглашение

--- app/src/main/java/ru/mvrlrd/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,10 @@
 package ru.mvrlrd.playlistmaker
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.Toolbar
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -11,6 +12,23 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val supportButton = findViewById<ImageButton>(R.id.support_button)
+        supportButton.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse("mailto:")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, R.string.developer_email)
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, R.string.email_subject)
+            emailIntent.putExtra(Intent.EXTRA_TEXT, R.string.email_text)
+            startActivity(emailIntent)
+        }
+
+        val agreementButton = findViewById<ImageButton>(R.id.agreement_button)
+        agreementButton.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW)
+            agreementIntent.data = Uri.parse(resources.getString(R.string.praktikum_link))
+            startActivity(agreementIntent)
+        }
     }
 
 
