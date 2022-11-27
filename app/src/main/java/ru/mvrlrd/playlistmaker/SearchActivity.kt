ebdf6ad918ccdfd4724fccefc3f27a5c9c9547af
mvrlrd <mvrlrd@yandex.ru>
2022-11-27 14:13:14 +0300

    реализовал строку поиска в поисковом активити

--- app/src/main/java/ru/mvrlrd/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/SearchActivity.kt
@@ -2,10 +2,40 @@ package ru.mvrlrd.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val clearIcon = findViewById<ImageView>(R.id.clearIcon)
+
+        clearIcon.setOnClickListener {
+            searchEditText.text.clear()
+        }
+
+        val simpleTextWatcher = object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                clearIcon.visibility = clearButtonVisibility(p0)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        }
+        searchEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    private fun clearButtonVisibility(p0: CharSequence?) = if (p0.isNullOrEmpty()) View.GONE else View.VISIBLE
 }
