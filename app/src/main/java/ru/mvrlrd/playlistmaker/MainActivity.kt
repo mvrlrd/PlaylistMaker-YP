7a68f11b456fc677dc215a64914e4cd881e73fcf
mvrlrd <mvrlrd@yandex.ru>
2023-02-13 23:55:26 +0300

    исправил навигацию

--- app/src/main/java/ru/mvrlrd/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/MainActivity.kt
@@ -4,29 +4,36 @@ package ru.mvrlrd.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var searchButton: Button
+    private lateinit var mediaButton: Button
+    private lateinit var settingsButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val searchButton = findViewById<Button>(R.id.search_button)
-        val onSearchButtonListener = View.OnClickListener {
-            val displayIntent = Intent(this, SearchActivity::class.java)
-            startActivity(displayIntent)
+        searchButton = findViewById<Button>(R.id.search_button).apply {
+            setOnClickListener {
+                navigateTo(SearchActivity::class.java)
+            }
         }
-        searchButton.setOnClickListener(onSearchButtonListener)
-        val mediaButton = findViewById<Button>(R.id.mediateka_button)
-        mediaButton.setOnClickListener {
-            val displayIntent = Intent(this, MediatekaActivity::class.java)
-            startActivity(displayIntent)
+        mediaButton = findViewById<Button>(R.id.mediateka_button).apply {
+            setOnClickListener {
+                navigateTo(MediatekaActivity::class.java)
+            }
         }
-        val settingsButton = findViewById<Button>(R.id.settings_button)
-        settingsButton.setOnClickListener {
-            val displayIntent = Intent(this, SettingsActivity::class.java)
-            startActivity(displayIntent)
+        settingsButton = findViewById<Button>(R.id.settings_button).apply {
+            setOnClickListener {
+                navigateTo(SettingsActivity::class.java)
+            }
         }
+    }
+
+
+    private fun navigateTo(clazz: Class<out AppCompatActivity>) {
+        val intent = Intent(this, clazz)
+        startActivity(intent)
     }
 }
