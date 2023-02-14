825beca37d881aa10dacc9f9e1465961289c2975
mvrlrd <mvrlrd@yandex.ru>
2023-02-14 18:08:21 +0300

    заменил  ImageButton на ImageView на SettingsActivity.kt

--- app/src/main/java/ru/mvrlrd/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/SettingsActivity.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
-import android.widget.ImageButton
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
@@ -17,6 +17,10 @@ class SettingsActivity : AppCompatActivity() {
 
     private lateinit var toolbar: Toolbar
     private lateinit var switchTheme : SwitchCompat
+    private lateinit var shareContainer: ViewGroup
+    private lateinit var supportContainer: ViewGroup
+    private lateinit var agreementContainer: ViewGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -25,48 +29,53 @@ class SettingsActivity : AppCompatActivity() {
             setNavigationOnClickListener { onBackPressed() }
         }
 
+        switchTheme = findViewById<SwitchCompat?>(R.id.switchTheme).apply {
+            setOnCheckedChangeListener { _, isChecked ->
+                val editor = getSharedPreferences(SWITCH_ENABLED, MODE_PRIVATE).edit()
+                if (isChecked) {
+                    editor.putBoolean(SWITCH_CONDITION, true)
+                    editor.apply()
+                    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+                } else {
+                    editor.putBoolean(SWITCH_CONDITION, false)
+                    editor.apply()
+                    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+                }
+            }
+        }.also {
+            it.isChecked = isDarkModeOn()
+        }
 
-        switchTheme = findViewById(R.id.switchTheme)
-        when (isDarkModeOn()) {
-            true -> {switchTheme.isChecked = true}
-           false -> {switchTheme.isChecked = false}
-        }
-        switchTheme.setOnCheckedChangeListener { _, isChecked ->
-            val editor = getSharedPreferences(SWITCH_ENABLED, MODE_PRIVATE).edit()
-            if (isChecked) {
-                editor.putBoolean(SWITCH_CONDITION, true)
-                editor.apply()
-                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
-            } else {
-                editor.putBoolean(SWITCH_CONDITION, false)
-                editor.apply()
-                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+
+        shareContainer = findViewById<ViewGroup?>(R.id.shareContainer).apply {
+            setOnClickListener {
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.praktikum_link))
+                    type = "text/plain"
+                }
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(shareIntent)
             }
         }
-        val supportButton = findViewById<ImageButton>(R.id.support_button)
-        supportButton.setOnClickListener {
-            val emailIntent = Intent(Intent.ACTION_SENDTO)
-            emailIntent.data = Uri.parse("mailto:")
-            emailIntent.putExtra(Intent.EXTRA_EMAIL, R.string.developer_email)
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT, R.string.email_subject)
-            emailIntent.putExtra(Intent.EXTRA_TEXT, R.string.email_text)
-            startActivity(emailIntent)
-        }
-        val agreementButton = findViewById<ImageButton>(R.id.agreement_button)
-        agreementButton.setOnClickListener {
-            val agreementIntent = Intent(Intent.ACTION_VIEW)
-            agreementIntent.data = Uri.parse(resources.getString(R.string.praktikum_link))
-            startActivity(agreementIntent)
-        }
-        val shareButton = findViewById<ImageButton>(R.id.shareButton)
-        shareButton.setOnClickListener {
-            val sendIntent: Intent = Intent().apply {
-                action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.praktikum_link))
-                type = "text/plain"
+
+        supportContainer = findViewById<ViewGroup?>(R.id.supportContainer).apply {
+            setOnClickListener {
+                val emailIntent = Intent(Intent.ACTION_SENDTO)
+                emailIntent.data = Uri.parse("mailto:")
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, R.string.developer_email)
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, R.string.email_subject)
+                emailIntent.putExtra(Intent.EXTRA_TEXT, R.string.email_text)
+                startActivity(emailIntent)
             }
-            val shareIntent = Intent.createChooser(sendIntent, null)
-            startActivity(shareIntent)
+        }
+
+        agreementContainer = findViewById<ViewGroup?>(R.id.agreementContainer).apply {
+            setOnClickListener {
+                val agreementIntent = Intent(Intent.ACTION_VIEW)
+                agreementIntent.data = Uri.parse(resources.getString(R.string.praktikum_link))
+                startActivity(agreementIntent)
+            }
         }
     }
 
