50e8c49bb6869c0b3afa3dae13ddb28d015a0ca8
mvrlrd <mvrlrd@yandex.ru>
2023-04-28 16:07:23 +0300

    убрал андроид класс из домэйн слоя

--- app/src/main/java/ru/mvrlrd/playlistmaker/settings/data/ThemeSwitchRepositoryImpl.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/settings/data/ThemeSwitchRepositoryImpl.kt
@@ -1,15 +1,25 @@
 package ru.mvrlrd.playlistmaker.settings.data
 
+import androidx.appcompat.app.AppCompatDelegate
 import ru.mvrlrd.playlistmaker.settings.domain.ThemeSwitchRepository
 
 class ThemeSwitchRepositoryImpl(private val themeStorage: IThemeStorage):ThemeSwitchRepository {
     override fun switchTheme(darkThemeEnabled: Boolean) {
         themeStorage.switch(darkThemeEnabled)
+        applyCurrentTheme()
     }
 
     override fun isDarkModeOn(): Boolean {
         return themeStorage.isDarkModeOn()
     }
 
-
+    override fun applyCurrentTheme() {
+        AppCompatDelegate.setDefaultNightMode(
+            if (isDarkModeOn()) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+    }
 }
--- app/src/main/java/ru/mvrlrd/playlistmaker/settings/domain/ThemeSwitchRepository.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/settings/domain/ThemeSwitchRepository.kt
@@ -3,4 +3,5 @@ package ru.mvrlrd.playlistmaker.settings.domain
 interface ThemeSwitchRepository {
     fun switchTheme(darkThemeEnabled: Boolean)
     fun isDarkModeOn(): Boolean
+    fun applyCurrentTheme()
 }
