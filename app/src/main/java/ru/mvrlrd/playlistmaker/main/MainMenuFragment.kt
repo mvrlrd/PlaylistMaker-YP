76800230a1fbbae92d02c8c20afd107c53d0b6c4
mvrlrd <mvrlrd@yandex.ru>
2023-06-10 13:20:59 +0300

    deleted mediatekaActivity

--- app/src/main/java/ru/mvrlrd/playlistmaker/main/MainMenuFragment.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/main/MainMenuFragment.kt
@@ -1,19 +1,13 @@
 package ru.mvrlrd.playlistmaker.main
 
-import android.content.Intent
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import ru.mvrlrd.playlistmaker.R
 import ru.mvrlrd.playlistmaker.databinding.FragmentMainMenuBinding
-import ru.mvrlrd.playlistmaker.mediateka.MediatekaActivity
 import ru.mvrlrd.playlistmaker.mediateka.MediatekaFragment
-import ru.mvrlrd.playlistmaker.search.ui.SearchActivity
-import ru.mvrlrd.playlistmaker.settings.ui.SettingsActivity
 
 class MainMenuFragment : Fragment() {
     private var _binding: FragmentMainMenuBinding? = null
