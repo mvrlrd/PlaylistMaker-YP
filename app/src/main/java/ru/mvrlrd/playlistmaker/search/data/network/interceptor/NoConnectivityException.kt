76fbae485a20d28e596226ed25d805f90257d978
mvrlrd <mvrlrd@yandex.ru>
2023-07-03 10:47:53 +0300

    ref

--- app/src/main/java/ru/mvrlrd/playlistmaker/search/data/network/interceptor/NoConnectivityException.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/search/data/network/interceptor/NoConnectivityException.kt
@@ -1,12 +1,12 @@
-package ru.mvrlrd.playlistmaker.search.data.network.interceptor
-
-import java.io.IOException
-
-class NoConnectivityException : IOException() {
-    override val message: String
-        get() = INTERNET_CONNECTION_MESSAGE
-
-    companion object{
-       private const val INTERNET_CONNECTION_MESSAGE = "No Internet Connection"
-    }
-}
+//package ru.mvrlrd.playlistmaker.search.data.network.interceptor
+//
+//import java.io.IOException
+//
+//class NoConnectivityException : IOException() {
+//    override val message: String
+//        get() = INTERNET_CONNECTION_MESSAGE
+//
+//    companion object{
+//       private const val INTERNET_CONNECTION_MESSAGE = "No Internet Connection"
+//    }
+//}
