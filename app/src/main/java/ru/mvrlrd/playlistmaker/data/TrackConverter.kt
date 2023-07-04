e6ec099c2481bf801d1db41ac3e84be50c14809c
mvrlrd <mvrlrd@yandex.ru>
2023-07-04 16:10:49 +0300

    created converter

---
+++ app/src/main/java/ru/mvrlrd/playlistmaker/data/TrackConverter.kt
@@ -0,0 +1,61 @@
+package ru.mvrlrd.playlistmaker.data
+import ru.mvrlrd.playlistmaker.player.domain.TrackForPlayer
+import ru.mvrlrd.playlistmaker.search.domain.Track
+
+class TrackConverter {
+    fun mapToEntity(track: Track) =
+        TrackEntity(
+            id = track.trackId,
+            trackName = track.trackName,
+            artistName = track.artistName,
+            trackTime = track.trackTime,
+            image = track.image,
+            album = track.album,
+            year = track.year,
+            genre = track.genre,
+            country = track.country,
+            previewUrl = track.previewUrl
+        )
+
+    fun mapToEntity(track: TrackForPlayer) =
+        TrackEntity(
+            id = track.trackId,
+            trackName = track.trackName,
+            artistName = track.artistName,
+            trackTime = track.trackTime,
+            image = track.image,
+            album = track.album,
+            year = track.year,
+            genre = track.genre,
+            country = track.country,
+            previewUrl = track.previewUrl
+        )
+
+    fun mapToPlayer(track: TrackEntity) =
+        TrackForPlayer(
+            trackId = track.id,
+            trackName = track.trackName,
+            artistName = track.artistName,
+            trackTime = track.trackTime,
+            image = track.image,
+            album = track.album,
+            year = track.year,
+            genre = track.genre,
+            country = track.country,
+            previewUrl = track.previewUrl
+        )
+
+    fun mapToAdapter(track: TrackEntity) =
+        TrackForPlayer(
+            trackId = track.id,
+            trackName = track.trackName,
+            artistName = track.artistName,
+            trackTime = track.trackTime,
+            image = track.image,
+            album = track.album,
+            year = track.year,
+            genre = track.genre,
+            country = track.country,
+            previewUrl = track.previewUrl
+        )
+}
