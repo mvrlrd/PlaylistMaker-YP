17b193486ec877af2abc56eec2f5f8326f6c853f
mvrlrd <mvrlrd@yandex.ru>
2023-07-04 15:51:29 +0300

    created TrackEntity

---
+++ app/src/main/java/ru/mvrlrd/playlistmaker/data/TrackEntity.kt
@@ -0,0 +1,19 @@
+package ru.mvrlrd.playlistmaker.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favorite_tracks")
+data class TrackEntity(
+    @PrimaryKey
+    val id: Int,
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String?,
+    val image: String,
+    val album: String,
+    val year: String?,
+    val genre: String,
+    val country: String,
+    val previewUrl: String?
+)
