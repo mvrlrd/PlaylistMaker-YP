5365868ea58a7c0020f5f96188265c7ece7dd3fa
mvrlrd <mvrlrd@yandex.ru>
2023-07-13 16:23:20 +0300

    hideBottomNav when in addplaylistfragment

--- app/src/main/java/ru/mvrlrd/playlistmaker/main/MainActivity.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/main/MainActivity.kt
@@ -3,10 +3,8 @@ package ru.mvrlrd.playlistmaker.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.mvrlrd.playlistmaker.R
@@ -20,20 +18,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.searchFragment, R.id.mediatekaFragment, R.id.settingsFragment
-//            )
-//        )
-
         navView.setupWithNavController(navController)
 
+        navController.addOnDestinationChangedListener{_,destination,_->
+            when (destination.id){
+                R.id.addPlaylistFragment -> hideBottomNav()
+                R.id.mediatekaFragment -> showBottomNav()
+            }
+        }
+    }
+
+    private fun hideBottomNav(){
+            binding.navView.visibility = View.GONE
+    }
+    private fun showBottomNav(){
+        binding.navView.visibility = View.VISIBLE
     }
 }
 
