97527d8af9db869d9d40684a20ce731849409fff
mvrlrd <mvrlrd@yandex.ru>
2023-07-16 19:58:11 +0300

    filter tracks with null preview url

--- app/src/main/java/ru/mvrlrd/playlistmaker/search/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/search/data/network/RetrofitNetworkClient.kt
@@ -11,6 +11,7 @@ import ru.mvrlrd.playlistmaker.R
 import ru.mvrlrd.playlistmaker.search.data.NetworkClient
 import ru.mvrlrd.playlistmaker.search.data.Response
 import ru.mvrlrd.playlistmaker.search.data.TracksSearchRequest
+import ru.mvrlrd.playlistmaker.search.data.model.TrackDto
 
 class RetrofitNetworkClient(
     private val itunesService: ItunesApiService,
@@ -32,10 +33,9 @@ class RetrofitNetworkClient(
         return withContext(Dispatchers.IO) {
             try {
                 val response = itunesService.search(dto.query)
+
                 response.apply { resultCode = context.resources.getString(R.string.success_code).toInt()
-                    response.results.forEach {
-                        Log.d("RetrofitNetworkClient", "${it}")
-                    }
+                    results = results.filter { it.previewUrl != null } as ArrayList<TrackDto>
 
                 }
             } catch (e: Throwable) {
