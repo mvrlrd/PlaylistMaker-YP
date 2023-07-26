2c89f35300d20c0e536701d9d783ef8f78e63609
mvrlrd <mvrlrd@yandex.ru>
2023-07-26 15:17:16 +0300

    вынес константы в стринг

--- app/src/main/java/ru/mvrlrd/playlistmaker/mediateka/playlists/edit_playlist/ui/EditPlaylistFragment.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/mediateka/playlists/edit_playlist/ui/EditPlaylistFragment.kt
@@ -1,18 +1,13 @@
 package ru.mvrlrd.playlistmaker.mediateka.playlists.edit_playlist.ui
 
 import android.os.Bundle
-import android.os.Environment
-import android.util.Log
 import android.view.View
-import androidx.core.content.res.ResourcesCompat
 import androidx.navigation.fragment.navArgs
-import ru.mvrlrd.playlistmaker.R
 import ru.mvrlrd.playlistmaker.mediateka.playlists.PlaylistEditingBaseFragment
 import ru.mvrlrd.playlistmaker.mediateka.playlists.domain.PlaylistForAdapter
-import ru.mvrlrd.playlistmaker.mediateka.playlists.playlists_screen.ui.PlaylistsFragment
-import ru.mvrlrd.playlistmaker.tools.loadPlaylist
-import java.io.File
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.mvrlrd.playlistmaker.R
+import ru.mvrlrd.playlistmaker.tools.loadPlaylistImageNEW
 
 class EditPlaylistFragment : PlaylistEditingBaseFragment() {
     private val args by navArgs<EditPlaylistFragmentArgs>()
@@ -24,34 +19,16 @@ class EditPlaylistFragment : PlaylistEditingBaseFragment() {
     }
 
     private fun setTitles() {
-        if (args.playlist.playlistImagePath.isNotEmpty()) {
-            try {
-                val filePath = File(
-                    requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES),
-                    resources.getString(R.string.my_album_name)
-                )
-                val file = File(filePath, args.playlist.playlistImagePath)
-                binding.ivNewPlaylistImage.loadPlaylist(
-                    file,
-                    PlaylistsFragment.PLAYLIST_IMAGE_SIZE
-                )
-            } catch (e: Exception) {
-                Log.e("PlaylistsFragment", e.message.toString())
-            }
-        }else{
-            binding.ivNewPlaylistImage.setImageDrawable(ResourcesCompat.getDrawable(resources,R.drawable.album_placeholder_image, requireActivity().theme ))
-        }
+        binding.ivNewPlaylistImage.loadPlaylistImageNEW(args.playlist.playlistImagePath)
         binding.ietPlaylistName.setText(args.playlist.name)
         binding.ietDesctiption.setText(args.playlist.description)
-        binding.btnCreatePlaylist.text = "Сохранить"
-        binding.btnBack.title = "Редактировать"
+        binding.btnCreatePlaylist.text = resources.getText(R.string.edit_playlist_button)
+        binding.btnBack.title = resources.getText(R.string.edit_playlist_title)
     }
-
 
     override fun createPlaylist(): PlaylistForAdapter{
         val name = binding.ietPlaylistName.text.toString()
         val description = binding.ietDesctiption.text.toString()
-
         return PlaylistForAdapter(
             playlistId = args.playlist.playlistId,
             name = name,
