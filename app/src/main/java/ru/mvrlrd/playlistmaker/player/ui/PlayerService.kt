41bdad5000d960496f3a5a0066cbe92b14d2f5bd
mvrlrd <mvrlrd@yandex.ru>
2023-08-19 15:51:55 +0300

    сделал сервис foreground ом

--- app/src/main/java/ru/mvrlrd/playlistmaker/player/ui/PlayerService.kt
+++ app/src/main/java/ru/mvrlrd/playlistmaker/player/ui/PlayerService.kt
@@ -1,10 +1,21 @@
 package ru.mvrlrd.playlistmaker.player.ui
 
+import android.R
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.Binder
+import android.os.Build
+import android.os.Bundle
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavDeepLinkBuilder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -12,10 +23,12 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
+
+import ru.mvrlrd.playlistmaker.main.MainActivity
 import ru.mvrlrd.playlistmaker.player.domain.PlayerInteractor
 import ru.mvrlrd.playlistmaker.player.domain.PlayerTrack
 import java.util.*
-
+import kotlin.properties.Delegates
 
 
 class PlayerService : Service() {
@@ -28,6 +41,13 @@ class PlayerService : Service() {
    private val _curr = MutableStateFlow(-1L)
     val curr get() = _curr.asStateFlow()
 
+    override fun onCreate() {
+        super.onCreate()
+
+        createNotificationChannel()
+        startForeground(NOTIFICATION_ID, createNotification())
+
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
@@ -52,6 +72,7 @@ class PlayerService : Service() {
     companion object{
         private const val TAG = "PlayerService"
         const val TRACK = "track"
+        private const val NOTIFICATION_ID = 1
 
          fun newIntent(context: Context, track: PlayerTrack): Intent{
             return Intent(context, PlayerService::class.java).apply {
@@ -68,4 +89,62 @@ class PlayerService : Service() {
     override fun onBind(intent: Intent): IBinder {
         return binder
     }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val notificationChannel = NotificationChannel(
+                12.toString(),
+                "DemoWorker",
+                NotificationManager.IMPORTANCE_DEFAULT,
+            )
+
+            val notificationManager: NotificationManager? =
+                ContextCompat.getSystemService(
+                    this.applicationContext,
+                    NotificationManager::class.java
+                )
+
+            notificationManager?.createNotificationChannel(
+                notificationChannel
+            )
+        }
+    }
+    private fun createNotification() : Notification {
+        createNotificationChannel()
+
+
+        val mainActivityIntent = Intent(
+            this.applicationContext,
+            MainActivity::class.java)
+
+
+        var pendingIntentFlag by Delegates.notNull<Int>()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            pendingIntentFlag = PendingIntent.FLAG_IMMUTABLE
+        } else {
+            pendingIntentFlag = PendingIntent.FLAG_UPDATE_CURRENT
+        }
+
+        val mainActivityPendingIntent = PendingIntent.getActivity(
+            this.applicationContext,
+            0,
+            mainActivityIntent,
+            pendingIntentFlag)
+
+
+        return NotificationCompat.Builder(
+            this.applicationContext,
+            "12"
+        )
+            .setSmallIcon(ru.mvrlrd.playlistmaker.R.drawable.ic_launcher_background)
+            .setContentTitle( this.applicationContext.getString(ru.mvrlrd.playlistmaker.R.string.app_name))
+            .setContentText("Work Request Done!")
+            .setContentIntent(mainActivityPendingIntent)
+            .setAutoCancel(true)
+            .build()
+    }
+
+
 }
